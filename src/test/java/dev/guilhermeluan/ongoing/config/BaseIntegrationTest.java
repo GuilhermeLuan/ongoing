0bf1d28f574203503150caabf2d464bd57057988
Guilherme Luan <guilhermeluan@pm.me>
2026-01-17 19:52:11 -0300

    feat(test): integrate PostgreSQL with Testcontainers
    * Add TestcontainersConfigurations for PostgreSQL setup
    * Update BaseIntegrationTest to use PostgreSQLContainer
    * Include spring-boot-testcontainers and testcontainers dependencies
    * Create application.yaml and application-test.yaml for configuration

--- src/test/java/dev/guilhermeluan/ongoing/config/BaseIntegrationTest.java
+++ src/test/java/dev/guilhermeluan/ongoing/config/BaseIntegrationTest.java
@@ -4,10 +4,11 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.context.ImportTestcontainers;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ImportTestcontainers(TestcontainersConfigurations.class)
 public abstract class BaseIntegrationTest {
-
     @LocalServerPort
     private int port;
 
---
+++ src/test/java/dev/guilhermeluan/ongoing/config/TestcontainersConfigurations.java
@@ -0,0 +1,13 @@
+package dev.guilhermeluan.ongoing.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+
+@TestConfiguration(proxyBeanMethods = false)
+public interface TestcontainersConfigurations {
+    @Container
+    @ServiceConnection
+    PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:15-alpine");
+}
