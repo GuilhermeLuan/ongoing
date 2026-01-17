6ceb3d49339f567cb4d957350920cbf0ed51a6c1
Guilherme Luan <guilhermeluan@pm.me>
2026-01-17 15:52:14 -0300

    first commit

---
+++ src/test/java/dev/guilhermeluan/ongoing/OngoingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.guilhermeluan.ongoing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OngoingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
