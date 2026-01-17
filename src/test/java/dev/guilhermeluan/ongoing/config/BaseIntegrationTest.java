1910cd045ffe6a9f9d8f5af114315a649b751f1a
Guilherme Luan <guilhermeluan@pm.me>
2026-01-17 18:27:33 -0300

    feat: config integration tests

---
+++ src/test/java/dev/guilhermeluan/ongoing/config/BaseIntegrationTest.java
@@ -0,0 +1,20 @@
+package dev.guilhermeluan.ongoing.config;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public abstract class BaseIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach()
+    void setUp() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+}
