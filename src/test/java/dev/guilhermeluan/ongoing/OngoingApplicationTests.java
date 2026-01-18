32e1a31c8bd34ca68ecf6cd68eb23af6182a89c5
Guilherme Luan <guilhermeluan@pm.me>
2026-01-18 13:26:35 -0300

    feat(flyway): add Flyway migration tests and enable Flyway * Enable Flyway in application-test.yaml * Add FlywayMigrationTest to verify migrations are applied * Update OngoingApplicationTests to use Testcontainers

--- src/test/java/dev/guilhermeluan/ongoing/OngoingApplicationTests.java
+++ src/test/java/dev/guilhermeluan/ongoing/OngoingApplicationTests.java
@@ -1,9 +1,12 @@
 package dev.guilhermeluan.ongoing;
 
+import dev.guilhermeluan.ongoing.config.TestcontainersConfigurations;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.context.ImportTestcontainers;
 
 @SpringBootTest
+@ImportTestcontainers(TestcontainersConfigurations.class)
 class OngoingApplicationTests {
 
 	@Test
---
+++ src/test/java/dev/guilhermeluan/ongoing/flywayMigration/FlywayMigrationTest.java
@@ -0,0 +1,24 @@
+package dev.guilhermeluan.ongoing.flywayMigration;
+
+import dev.guilhermeluan.ongoing.config.BaseIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class FlywayMigrationTest extends BaseIntegrationTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    void shouldHaveAppliedMigrations() {
+        Integer migrationCount = jdbcTemplate.queryForObject(
+                "SELECT count(*) FROM flyway_schema_history", Integer.class
+        );
+
+        assertThat(migrationCount)
+                .isGreaterThan(0);
+    }
+}
