3f9fe9586e074fb975cc81e2008b55b3bdf7a46d
Guilherme Luan <guilhermeluan@pm.me>
2026-01-20 21:16:23 -0300

    feat(subscriptions): add exception handling for not found subscriptions
    * Introduced SubscriptionNotFoundException for handling not found cases.
    * Implemented SubscriptionExceptionHandler to return structured error responses.
    * Utilized ProblemDetail for standardized error reporting.

--- src/main/java/dev/guilhermeluan/ongoing/subscriptions/SubscriptionNotFoundException.java
+++ src/main/java/dev/guilhermeluan/ongoing/subscriptions/SubscriptionNotFoundException.java
@@ -1,5 +1,9 @@
 package dev.guilhermeluan.ongoing.subscriptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class SubscriptionNotFoundException extends RuntimeException {
 
     public SubscriptionNotFoundException(Long id) {
