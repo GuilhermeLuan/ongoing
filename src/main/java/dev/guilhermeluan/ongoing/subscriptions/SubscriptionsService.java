e11897b65ef7bc3a1299f42bac0e8049568c8979
Guilherme Luan <guilhermeluan@pm.me>
2026-01-23 08:46:11 -0300

    feat(subscriptions): add next billing date calculation and tests
    * Implement calculateNextBillingDate method in SubscriptionsService
    * Update save method to set next payment date
    * Add unit tests for next billing date calculation
    * Validate billing cycle in the calculation method

--- src/main/java/dev/guilhermeluan/ongoing/subscriptions/SubscriptionsService.java
+++ src/main/java/dev/guilhermeluan/ongoing/subscriptions/SubscriptionsService.java
@@ -1,10 +1,12 @@
 package dev.guilhermeluan.ongoing.subscriptions;
 
 import dev.guilhermeluan.ongoing.exception.NotFoundException;
+import dev.guilhermeluan.ongoing.subscriptions.entities.BillingCycle;
 import dev.guilhermeluan.ongoing.subscriptions.entities.Subscriptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -14,6 +16,10 @@ public class SubscriptionsService {
     public SubscriptionsService(SubscriptionsRepository subscriptionsRepository) {this.subscriptionsRepository = subscriptionsRepository;}
 
     public Subscriptions save(Subscriptions subscription) {
+
+        LocalDate nextBillingDate = calculateNextBillingDate(subscription);
+        subscription.setNextPaymentDate(nextBillingDate);
+
         return subscriptionsRepository.save(subscription);
     }
 
@@ -34,6 +40,24 @@ public class SubscriptionsService {
         Subscriptions existingSubscription = findByIdOrThrowNotFoundException(id);
         subscriptionToUpdate.setId(existingSubscription.getId());
         return subscriptionsRepository.save(subscriptionToUpdate);
+    }
+
+    public LocalDate calculateNextBillingDate(Subscriptions subscription) {
+        LocalDate lastBillingDate = subscription.getStartDate();
+        BillingCycle billingCycle = subscription.getBillingCycle();
+
+
+        if (billingCycle == null || billingCycle.getId() == null) {
+            throw new IllegalArgumentException("Billing cycle is required");
+        }
+
+
+        if (billingCycle.getId() == 1) {
+            return lastBillingDate.plusMonths(1);
+        } else if (billingCycle.getId() == 2) {
+            return lastBillingDate.plusYears(1);
+        }
+        throw new IllegalArgumentException("Unknown billing cycle: " + subscription.getBillingCycle());
     }
 
 }
